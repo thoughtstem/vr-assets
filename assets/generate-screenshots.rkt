8057ba0ec194a74659b09a7f130d5b1bc4fef6a7
srfoster <stephen@thoughtstem.com>
2019-09-20 13:01:50 -0700

    fixing test

--- assets/generate-screenshots.rkt
+++ assets/generate-screenshots.rkt
@@ -3,16 +3,18 @@
 ;Note: You'll need to install screenshot-glb
 ;  https://github.com/Shopify/screenshot-glb
 
-(fold-files
-  (lambda (f type _2)
-    (when (eq? type 'file)
-      (define base (first (string-split (~a f) ".")))
-      (define type (second (string-split (~a f) ".")))
+(module+ main
 
-      (when (string=? type "glb")
-        (define f-png (~a base ".png"))
+  (fold-files
+    (lambda (f type _2)
+      (when (eq? type 'file)
+        (define base (first (string-split (~a f) ".")))
+        (define type (second (string-split (~a f) ".")))
 
-        (displayln f)
-        (system 
-          (~a "screenshot-glb -i " f " -o " f-png " -w 400 -h 400")))))
-  (void))
+        (when (string=? type "glb")
+          (define f-png (~a base ".png"))
+
+          (displayln f)
+          (system 
+            (~a "screenshot-glb -i " f " -o " f-png " -w 400 -h 400")))))
+    (void)))
--- assets/grab-model.rkt
+++ assets/grab-model.rkt
@@ -1,30 +1,33 @@
 #lang racket
 
-(require net/url)
+(module+ main
+  (require net/url)
 
-;Note: You'll need to install screenshot-glb
-;  https://github.com/Shopify/screenshot-glb
+  ;Note: You'll need to install screenshot-glb
+  ;  https://github.com/Shopify/screenshot-glb
 
 
-(define url-string (make-parameter #f))
-(define out-file (make-parameter #f)) 
+  (define url-string (make-parameter #f))
+  (define out-file (make-parameter #f)) 
 
-(define args (current-command-line-arguments))
+  (define args (current-command-line-arguments))
 
-(url-string (vector-ref args 0))
-(out-file (vector-ref args 1))
+  (url-string (vector-ref args 0))
+  (out-file (vector-ref args 1))
 
-(define base
-  (first (string-split (out-file) ".")))
+  (define base
+    (first (string-split (out-file) ".")))
 
-(define out-file-png (~a base ".png"))
+  (define out-file-png (~a base ".png"))
 
-(define url (string->url (url-string)))
+  (define url (string->url (url-string)))
 
-(call-with-output-file (out-file) 
-  (lambda (p) 
-    (display (port->bytes (get-pure-port #:redirections 10 url)) p))
-  #:exists 'replace)
+  (call-with-output-file (out-file) 
+                         (lambda (p) 
+                           (display (port->bytes (get-pure-port #:redirections 10 url)) p))
+                         #:exists 'replace)
 
-(system 
-  (~a "screenshot-glb -i " (out-file) " -o " out-file-png " -w 400 -h 400"))
+  (system 
+    (~a "screenshot-glb -i " (out-file) " -o " out-file-png " -w 400 -h 400"))
+
+  )
